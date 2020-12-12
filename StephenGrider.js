d29efc8fad15ff3aa12a010d6788de8241968371
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2020-12-12 18:48:36 -0500

    first commit

---
+++ StephenGrider.js
@@ -0,0 +1,13 @@
+function palindrome (str) {
+    const reversed = str.split('').reverse().join('');
+
+    return str === reversed;
+}
+
+Alternate palindrome solution; 
+
+function palindrome(str) {
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
