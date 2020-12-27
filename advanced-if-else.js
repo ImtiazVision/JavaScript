b1faf269a381dd79f6484c44eee123e6ad4d5f4d
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2020-12-27 11:44:19 -0500

    update

---
+++ advanced-if-else.js
@@ -0,0 +1,8 @@
+let temp = 45
+if (temp <= 32) {
+    console.log("It's freezing outside!")
+} else if (temp >= 100 ) {
+    console.log("It is hot outside, take precausions")
+} else {
+    console.log("It's nice to go outside and enjoy")
+}
