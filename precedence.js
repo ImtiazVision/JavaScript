05540fbb3c14d8429b4fa2a05363e7235c90899f
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2020-12-28 19:32:32 -0500

    update

---
+++ precedence.js
@@ -0,0 +1,18 @@
+var temp = 3;
+console.log("temp:", temp)
+
+var a = 3;
+var b = 4;
+var c = 5;
+
+b = c;
+console.log(b)
+c = a;
+console.log(c)
+a = b;
+console.log(a)
+c = b; 
+console.log(c)
+
+var a = 2 + 3 * 4 / 2 * (3-2);
+console.log(a)
