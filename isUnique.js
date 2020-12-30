f844af08eb02e75f2e285c29a68b02b66c7f5f1c
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2020-12-30 18:51:54 -0500

    update

--- isUnique.js
+++ isUnique.js
@@ -1,3 +1,4 @@
+// First Solution
 function isUnique(str) {
     for(let i = 0; i < str.length; i++) {
         if(str.lastIndexOf(str[i]) !== i) {
@@ -9,3 +10,20 @@ function isUnique(str) {
 }
 string1 = isUnique('Mary')
 console.log(string1)
+
+// 2nd Solution
+
+function isUnique(str) {
+    const characters = str.split('').sort();
+
+    for (let i = 1; i < characters.length; i++) {
+        if (characters[i] === characters[i - 1]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+string2 = isUnique('Mariam')
+console.log(string2)
