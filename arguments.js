349e9aae439025a65b35300272b782bd483e1032
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2020-12-31 19:11:09 -0500

    update

--- arguments.js
+++ arguments.js
@@ -1,6 +1,16 @@
+// Multiple arguments
 let add = function (x,y,z) {
     return x + y + z
 };
 
 let result = add(1,3, 4);
 console.log(result);
+
+// Default arguments
+let getScoreText = function(name = 'Anonymous', score = 0) {
+    // console.log(name)
+    // console.log(score)
+    return 'Name: ' + name + ' | Score: ' + score
+}
+let scoreTest = getScoreText(undefined, 40)
+console.log(scoreTest)
