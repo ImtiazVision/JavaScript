90dacb64230f5310806d4b8f3036084c3c20a614
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2021-01-01 17:52:12 -0500

    update

--- functions/templateString.js
+++ functions/templateString.js
@@ -2,3 +2,16 @@ let name = 'Kevin';
 let occupation = 'Web Developer';
 
 console.log(`Hello, my name is ${name} and I am a ${occupation}!`);
+
+// Challenge Area 
+// A 20% tip on $50 would be $10
+
+
+let billCalculator = function (total, tipPercent = .2) {
+    let percent = tipPercent * 100
+    let tip = total * tipPercent
+    return `A ${percent}% tip on $${total} would be $${tip} `
+}
+
+let totalBill = billCalculator(100);
+console.log(totalBill);
