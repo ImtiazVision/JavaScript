8f6695bdf70271ea40ba2ce49ea75cfe3cd9bffd
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2021-02-02 16:54:58 -0500

    'added a new file'

---
+++ CodeWithMosh/even-odd-numbers.js
@@ -0,0 +1,9 @@
+showNum(10);
+
+function showNum(num) {
+    for (let i = 0; i <= num; i++) {
+        if (i%2 === 0) console.log(i,'even number')
+        else console.log(i,'odd number')
+
+    }
+}
