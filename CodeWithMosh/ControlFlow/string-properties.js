1693a57bfcd2cf872de7d9fa77501e8f4ce6c2b2
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2021-02-06 21:50:52 -0500

    update

--- CodeWithMosh/ControlFlow/string-properties.js
+++ CodeWithMosh/ControlFlow/string-properties.js
@@ -11,6 +11,7 @@ showProperties(movie)
 
 function showProperties(obj) {
     for (let key in obj)
-        console.log(key, obj[key]);
+        if (typeof obj[key] === 'string')
+            console.log(key, obj[key]);
 
 }
