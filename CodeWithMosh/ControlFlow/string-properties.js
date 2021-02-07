3dfacbfe5b07b756d2213ed928bca40ed1170183
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2021-02-06 21:35:46 -0500

    update

---
+++ CodeWithMosh/ControlFlow/string-properties.js
@@ -0,0 +1,16 @@
+const movie = {
+    title: 'Superman',
+    releaseYear: 2019,
+    rating: 9,
+    director: 'Tom'
+
+};
+
+
+showProperties(movie)
+
+function showProperties(obj) {
+    for (let key in obj)
+        console.log(key)
+
+}
