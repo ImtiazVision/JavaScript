5e73f7228de99c10d1f4abe87718f180727e9ec6
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2021-02-06 19:54:57 -0500

    update

---
+++ CodeWithMosh/ControlFlow/countTruthy.js
@@ -0,0 +1,16 @@
+const isActive = true;
+const name1 = 'Imtiaz';
+const name2 = ''        // empty string is a falsy value // 
+if (name2) console.log('Hello')
+if (name1) console.log('Imtiaz')
+if (isActive) console.log('Active')
+
+
+
+
+
+
+
+function countTruthy(array) {
+
+}
