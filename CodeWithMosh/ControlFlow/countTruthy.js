a3ff3ade03900cba86db78199bf98a42d28b2c6a
Imtiaz <ahmedimtiaz.ahmed@gmail.com>
2021-02-06 21:24:18 -0500

    update

--- CodeWithMosh/ControlFlow/countTruthy.js
+++ CodeWithMosh/ControlFlow/countTruthy.js
@@ -14,5 +14,10 @@ console.log(countTruthy(array));
 
 
 function countTruthy(array) {
+    let count = 0;
+    for (let value of array)
+        if (value)
+            count++;
+        return count;
 
 }
